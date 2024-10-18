978c780070ecea2b859c0e284cdc488db6eadde4
上官令 <2437167255@qq.com>
2024-10-18 19:10:44 +0800

    加入preview文件夹

--- script/generateMapUpdate.py
+++ script/generateMapUpdate.py
@@ -4,6 +4,7 @@ from datetime import date
 
 import pandas as pd
 
+
 def get_map_full(directory):
     """
     获取指定目录下所有图片文件的文件名（不包含后缀）。
@@ -12,71 +13,135 @@ def get_map_full(directory):
     :return: 包含图片文件名（不含后缀）的列表
     """
     # 图片文件的常见后缀
-    image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp',".webp"]
-    
+    image_extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp"]
+
     # 获取目录下所有文件名
     files = os.listdir(directory)
-    # file_paths = 
+    # file_paths =
     # for i in [os.path.join(directory, file) for file in files]:
     #     file_paths.append(i.replace("\\", "/"))
     # print(file_paths)
     # 过滤出图片文件，并移除后缀
-    map_full = [os.path.splitext(file)[0] for file in files if os.path.splitext(file)[1].lower() in image_extensions]
-    
-    return map_full,files
+    map_full = [
+        os.path.splitext(file)[0]
+        for file in files
+        if os.path.splitext(file)[1].lower() in image_extensions
+    ]
+
+    return map_full, files
+
 
 def get_map_data_list_from_excel(excel_file_path):
-    df_selected_columns = pd.read_excel(excel_file_path, usecols=['地图名称', '实际宽',"实际高","高度缩放","更新时间"])
-    df_selected_columns['更新时间'] = df_selected_columns['更新时间'].dt.strftime('%Y-%m-%d %H:%M:%S')
+    df_selected_columns = pd.read_excel(
+        excel_file_path,
+        usecols=[
+            "地图名称_英文",
+            "地图名称_中文",
+            "实际宽",
+            "实际高",
+            "高度缩放",
+            "更新时间",
+        ],
+    )
+    df_selected_columns["更新时间"] = df_selected_columns["更新时间"].dt.strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
     return df_selected_columns.values
 
 
-if __name__ == '__main__':
-    map_full,map_file_full = get_map_full("Maps\\full")
-    map_lite,map_file_lite = get_map_full("Maps\\lite")
-    map_height,map_height_file = get_map_full("Maps\\heightmap")
+if __name__ == "__main__":
+    hostUrl = "https://gitee.com/squadtools/squad-tools-repository/raw/master"
+    fullMapPath = "Maps/full"
+    liteMapPath = "Maps/lite"
+    heightMapPath = "Maps/heightmap"
+    previewMapPath = "Maps/previews"
+    excelPath = "excel\\地图数据.xlsx"
 
-    map_data = get_map_data_list_from_excel("excel\\地图数据.xlsx")
-    data_dict =  {}
+    # 获取地图文件名
+    map_full, map_file_full = get_map_full(fullMapPath)
+    map_lite, map_file_lite = get_map_full(liteMapPath)
+    map_height, map_height_file = get_map_full(heightMapPath)
+    map_preview, map_preview_file = get_map_full(previewMapPath)
+    # 获取地图数据
+    map_data = get_map_data_list_from_excel(excelPath)
+    data_dict = {}
     map_data_list = []
-   
+
     if len(map_full) != len(map_data):
-        
-        print("地图数量不匹配",)
+        print("地图数量不匹配")
         exit(0)
-    
-    for i in range(len(map_full)):
-        for j in range(len(map_data)):
-            flag = False
-            for k in range(len(map_height)):
-                if map_full[i] == map_data[j][0] == map_lite[i] == map_height[k]:
-                    print(f"正在匹配{map_full[i]} {len(map_data_list)+1}")
-                    map_data_list.append({
-                        "map_name":map_full[i] ,
-                        "map_size":[max(int(map_data[j][1]),int(map_data[j][2])),max(int(map_data[j][1]),int(map_data[j][2])) ],
-                        "update_time":map_data[j][4],
-                        "map_pixmap_full_url":f"https://gitee.com/squadtools/squad-tools-repository/raw/master/Maps/full/{map_file_full[i]}",
-                        "map_pixmap_lite_url":f"https://gitee.com/squadtools/squad-tools-repository/raw/master/Maps/lite/{map_file_lite[i]}",
-                        "map_heightmap_url":f"https://gitee.com/squadtools/squad-tools-repository/raw/master/Maps/heightmap/{map_height_file[k]}",
-                        "map_height_factor":map_data[j][3]
-                    }) 
-                    flag = True
-                    break 
-            if flag: 
+    if len(map_preview) != len(map_data):
+        print("预览图数量不匹配")
+        exit(0)
+    if len(map_lite)!= len(map_data):
+        print("缩略图数量不匹配")
+        exit(0)
+    if len(map_height)!= len(map_data):
+        print("高度图数量不匹配")
+        exit(0)
+    # 检查空列表
+    # if not (map_full and map_data and map_lite and map_height and map_preview):
+    #     print("其中一个或多个地图列表为空，无法进行匹配。")
+    #     exit(0)
+
+    # 使用字典存储 map_data 的信息
+    map_data_dict = {data[0]: data for data in map_data}
+
+    # 优化后的遍历逻辑
+    for i, full_map in enumerate(map_full):
+        
+        if full_map not in map_data_dict:
+            print(f"未找到 {full_map} 对应的数据，跳过当前项。")
+            continue
+            
+        data = map_data_dict[full_map]
+        name_en = data[0]
+        name_zh = data[1]
+
+        try:
+            map_size = max(int(data[2]), int(data[3]))
+        except ValueError:
+            print(f"无法将 {data[2]} 或 {data[3]} 转换为整数，跳过当前项。")
+            continue
+
+        update_time = data[5]
+        height_factor = data[4]
+
+        # 查找对应的文件路径
+        full_pix_url = f"{hostUrl}/{fullMapPath}/{map_file_full[i]}"
+        lite_pix_url = f"{hostUrl}/{liteMapPath}/{map_file_lite[i]}"
+
+        height_map_url = None
+        for height_file in map_height:
+            if height_file == full_map:
+                height_map_url = f"{hostUrl}/{heightMapPath}/{map_height_file[map_height.index(height_file)]}"
                 break
+            
+
+        preview_map_url = None
+        for preview_file in map_preview:
+            if preview_file == full_map:
+                preview_map_url = f"{hostUrl}/{previewMapPath}/{map_preview_file[map_preview.index(preview_file)]}"
+                break
+            
         
-    data_dict["map_data_list"] = map_data_list     
-    with open("Json/map_data.json","w",encoding="utf-8") as f: 
-        json.dump(data_dict,f,ensure_ascii=False,indent=4)  
-    print("生成地图数据成功 匹配数量：",len(map_data_list))
+        if height_map_url and preview_map_url:
+            print(f"正在匹配 {full_map} {len(map_data_list) + 1}")
+            map_data_list.append(
+                {
+                    "name_en": name_en,
+                    "name_zh": name_zh,
+                    "map_size": map_size,
+                    "updateTime": update_time,
+                    "fullPixUrl": full_pix_url,
+                    "litePixUrl": lite_pix_url,
+                    "heightmapUrl": height_map_url,
+                    "previewMapUrl": preview_map_url,
+                    "heightFactor": height_factor,
+                }
+            )
 
-    
-        
-    
-
-    
-
-    
-
-
-
+    data_dict["map_data_list"] = map_data_list
+    with open("Json/map_data_update.json", "w", encoding="utf-8") as f:
+        json.dump(data_dict, f, ensure_ascii=False, indent=4)
+    print("生成地图数据成功 匹配数量：", len(map_data_list))
